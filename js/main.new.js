b2542d76e0d1a34428c40d5c2b30b1ce82ac097e
opusberedugo <46562000+opusberedugo@users.noreply.github.com>
2021-08-05 01:22:58 +0100

    Next Button First Phase

--- js/main.new.js
+++ js/main.new.js
@@ -1,7 +1,7 @@
 let questions = [];
 let questionBar = document.querySelector("p.question");
 let options = document.querySelectorAll("p.option");
-let controlButton = document.querySelectorAll(".columns button");
+let controlButtons = document.querySelectorAll(".columns button");
 let currentQuestion = 1;
 let score = 0;
 
@@ -13,7 +13,9 @@ const clearScreen = () => {
   questionBar.textContent = "";
   options.forEach((v) => {
     v.textContent = ""
+    v.classList.remove("selected")
   })
+
 }
 
 const getQuestion = () => {
@@ -23,6 +25,8 @@ const getQuestion = () => {
     // console.log(data.results);
     questions[questions.length] = data.results;
     questions = questions.flat();
+  }).catch((e) => {
+    console.log(e);
   }).finally(() => {
     questionBar.innerHTML = questions[questions.length - 1].question;
     options[0].innerHTML = questions[questions.length - 1].correct_answer
@@ -50,29 +54,84 @@ const previousQuestion = () => {
 
 }
 
+const getClassListString = (element) => {
+  let classListString = [];
+  element.classList.forEach((v) => {
+    classListString.push(v);
+  })
+  return classListString.join(" ");
+}
+
 getQuestion();
 
 for (const option of options) {
   option.onclick = () => {
-    clearScreen();
-    ++currentQuestion;
-    document.querySelector("p.progress").textContent = `${currentQuestion} / 20`;
-    getQuestion();
-
+    options.forEach((v) => { v.classList.remove("selected") })
+    option.classList.add("selected")
   }
 }
 
-options[0].onclick = () => {
-  clearScreen();
-  ++currentQuestion;
-  ++score;
-  document.querySelector("p.progress").textContent = `${currentQuestion} / 20`;
-  document.querySelector("p.point").textContent = `${score} point(s)`;
-  getQuestion();
-}
 
-controlButton[0].addEventListener("click", () => {
+
+controlButtons[0].addEventListener("click", () => {
+  if (getClassListString(options[0]).includes("selected")) {
+    score++;
+  }
   clearScreen();
   previousQuestion();
 
 })
+
+controlButtons[2].addEventListener("click", () => {
+  if (currentQuestion === questions.length) {
+    if (getClassListString(options[0]).includes("selected")) {
+      score += 1;
+    }
+    ++currentQuestion;
+    document.querySelector("p.point").textContent = `${score} point(s)`;
+    document.querySelector("p.progress").textContent = `${currentQuestion} / 5`;
+
+    clearScreen();
+    getQuestion();
+
+
+  } else if (currentQuestion != questions.length) {
+    ++currentQuestion;
+    clearScreen();
+
+    if (getClassListString(options[0]).includes("selected")) {
+      score += 1;
+    }
+
+    questionBar.innerHTML = questions[currentQuestion].question;
+    options[0].innerHTML = questions[currentQuestion].correct_answer
+
+    for (let i = 0; i < questions[currentQuestion].incorrect_answers.length; i++) {
+      options[i + 1].innerHTML = questions[currentQuestion].incorrect_answers[i];
+    }
+
+    document.querySelector("p.point").textContent = `${score} point(s)`;
+    document.querySelector("p.progress").textContent = `${currentQuestion} / 20`;
+
+  }
+
+})
+
+
+controlButtons[0].onclick = () => {
+
+  if (currentQuestion === 1) {
+    controlButtons[0].disabled = true
+  } else {
+    --currentQuestion;
+    --score;
+    clearScreen();
+    questionBar.innerHTML = questions[currentQuestion].question;
+    options[0].innerHTML = questions[currentQuestion].correct_answer
+
+    for (let i = 0; i < questions[currentQuestion].incorrect_answers.length; i++) {
+      options[i + 1].innerHTML = questions[currentQuestion].incorrect_answers[i];
+    }
+  }
+
+}
